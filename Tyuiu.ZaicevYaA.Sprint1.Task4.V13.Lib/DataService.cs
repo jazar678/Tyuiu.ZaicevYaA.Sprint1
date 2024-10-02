1ef961588210c324da68fb29dd17eb7b4bad3291
Redeks Gg <ddredeks@gmail.com>
2024-10-03 01:51:55 +0500

    Task4

---
+++ Tyuiu.ZaicevYaA.Sprint1.Task4.V13.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.ZaicevYaA.Sprint1.Task4.V13.Lib
+{
+    public class DataService : ISprint1Task4V13
+    {
+        public double Calculate(double x, double y)
+        {
+            double pi = Math.PI;
+            return Math.Round((Math.Cos(pi/x) / Math.Exp(x + y)) , 3);
+        }
+    }
+}
---
+++ Tyuiu.ZaicevYaA.Sprint1.Task4.V13/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.ZaicevYaA.Sprint1.Task4.V13.Lib;
+namespace Tyuiu.ZaicevYaA.Sprint1.Task4.V13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт | Выполнил: Зайцев Я.А. | ПКТб-24-1";
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("* Спринт #1                                                     *");
+            Console.WriteLine("* Тема: Базовые навыки работы C#                                *");
+            Console.WriteLine("* Задание #4                                                    *");
+            Console.WriteLine("* Вариант #13                                                   *");
+            Console.WriteLine("* Выполнил : Зайцев Ярослав Александрович | ПКТб-24-1           *");
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("* УСЛОВИЕ :                                                     *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение               *");
+            Console.WriteLine("* и печатает результат на экране                                *");
+            Console.WriteLine("*                                                               *");
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ :                                             *");
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("* cos(pi/x)/E^(y+x)                                             *");
+            Console.WriteLine("* Введите X                                                     *");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("* Введите Y                                                     *");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                    *");
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine(ds.Calculate(x, y));
+            Console.ReadKey();
+        }
+    }
+}
