1fa647ba017ba9fa26c7e0f62183e0ef2ffd4fc7
Redeks Gg <ddredeks@gmail.com>
2024-10-03 02:04:03 +0500

    Task4

--- Tyuiu.ZaicevYaA.Sprint1.Task4.V13.Lib/DataService.cs
+++ Tyuiu.ZaicevYaA.Sprint1.Task4.V13.Lib/DataService.cs
@@ -5,8 +5,8 @@ namespace Tyuiu.ZaicevYaA.Sprint1.Task4.V13.Lib
     {
         public double Calculate(double x, double y)
         {
-            double pi = Math.PI;
-            return Math.Round((Math.Cos(pi/x) / Math.Exp(x + y)) , 3);
+            double pi = Math.Round( Math.PI,2);
+            return Math.Round((Math.Cos(pi/x) / (3 * Math.Exp(x + y))) , 3);
         }
     }
 }
