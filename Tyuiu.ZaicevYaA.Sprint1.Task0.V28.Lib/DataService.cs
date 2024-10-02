d66661646efb7b3e45d977d220ab8f7ad9b25d90
Redeks Gg <ddredeks@gmail.com>
2024-10-02 22:00:09 +0500

    Task0

--- Tyuiu.ZaicevYaA.Sprint1.Task0.V28.Lib/DataService.cs
+++ Tyuiu.ZaicevYaA.Sprint1.Task0.V28.Lib/DataService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using tyuiu.cources.programming.interfaces.Sprint1;
 namespace Tyuiu.ZaicevYaA.Sprint1.Task0.V28.Lib
 {
-    public class DataService : ISprint1Task0V0
+    public class DataService : ISprint1Task0V28
     {
         public double Calculate()
         {
